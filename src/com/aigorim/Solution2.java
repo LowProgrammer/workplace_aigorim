dc8711336b54815479272dc9648ebbd3c8a6a0a8
陈群飞 <1320954152@qq.com>
2019-10-09 16:06:23 +0800

    solution2添加

---
+++ src/com/aigorim/Solution2.java
@@ -0,0 +1,14 @@
+package com.aigorim;
+
+/**
+ * @author feifei
+ * @Classname Solution2
+ * @Description TODO
+ * @Date 2019/10/9 16:00
+ * @Created by 陈群飞
+ */
+public class Solution2 {
+    public static void main(String[] args) {
+        System.out.println(232356);
+    }
+}
