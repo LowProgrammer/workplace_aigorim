25e7eb6ab11ebab6e91bdb6f4ae84575cfce8a6f
陈群飞 <1320954152@qq.com>
2019-10-11 10:59:06 +0800

    leetcode 999. Available Captures for Rook

--- src/com/aigorim/Solution2.java
+++ src/com/aigorim/Solution2.java
@@ -11,7 +11,10 @@ public class Solution2 {
     public static void main(String[] args) {
         System.out.println(232356);
         Solution2 s=new Solution2();
-        s.countBits(16);
+        int[] arr=s.countBits(129);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+"  ");
+        }
     }
 
     /**
@@ -24,7 +27,6 @@ public class Solution2 {
     public int[] countBits(int num) {
         int[] arr=new int[num+1];
         int x=1;
-
         for (int i = 0; i <=num; i++) {
             if (i<2){
                 getNum(arr,i,0);
@@ -39,7 +41,13 @@ public class Solution2 {
         return arr;
     }
 
-    public void getNum(int[] arr,int n,int x){
+    /**
+     * 计算数n换成二进制数会有多少个1
+     * @param arr
+     * @param n
+     * @param x
+     */
+    private void getNum(int[] arr,int n,int x){
         if (n==0){
             arr[0]=0;
         }else if (n==1){
@@ -49,5 +57,44 @@ public class Solution2 {
         }else{
             arr[n]=1+arr[n-x];
         }
+    }
+
+    /**
+     * @author feifei
+     * @param board
+     * @Description TODO  leetcode 999. Available Captures for Rook
+     * @Date 2019/10/11 10:57
+     * 计算一个车在四个方向上能有多少个卒子能吃，使用数组来标记四个方向
+     *
+     */
+    public int numRookCaptures(char[][] board) {
+        int x = 0,y=0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j]=='R'){
+                    x=i;
+                    y=j;
+                    break;
+                }
+            }
+        }
+        int[][] di=new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+        int res=0;
+        for (int i = 0; i < 4; i++) {
+            int x0=x+di[i][0];
+            int y0=y+di[i][1];
+            while(x0>=0&&x0<8&&y0<8&&y0>=0){
+                if (board[x0][y0]=='p'){
+                    res++;
+                    break;
+                }
+                if (board[x0][y0]=='B'){
+                    break;
+                }
+                x0+=di[i][0];
+                y0+=di[i][1];
+            }
+        }
+        return res;
     }
 }
